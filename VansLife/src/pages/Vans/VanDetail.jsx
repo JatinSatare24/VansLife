b56effe8179f068a34ed4b12cafcec460c75f7ae
Jatin Satare <jatinsatare@gmail.com>
2026-03-17 15:52:23 +0530

    add sad path, error handling & loading state for van details

--- VansLife/src/pages/Vans/VanDetail.jsx
+++ VansLife/src/pages/Vans/VanDetail.jsx
@@ -14,15 +14,6 @@ export default function VanDetail() {
             .then(data => setVan(data.vans))
     }, [params.id])
 
-    /**
-     * Challenge: When a filter is applied, change the text of
-     * the button to say "Back to luxury vans" (e.g.) instead of
-     * "Back to all vans".
-     * 
-     * As usual, there's more than one way to solve this, so just
-     * give it your best shot
-     */
-    
     const search = location.state?.search || ""
     const type = location.state?.type || "all"
     
